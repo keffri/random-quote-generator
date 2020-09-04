30584b032d26af74f0a07016096248ca8518fc94
keffri <keffri.w.neal@gmail.com>
2020-09-04 13:07:10 -0700

    non-repeating quote functionality added

--- script.js
+++ script.js
@@ -5,37 +5,33 @@ let originator = document.querySelector(".originator");
 let quotes = [
   `"We suffer more often in imagination than in reality."`,
   `“The soul becomes dyed with the colour of its thoughts.”`,
-  `"Set aside a certain number of days, during which you shall be content with the scantiest and cheapest fare, with coarse and rough dress, saying to yourself the while: “Is this the condition that I feared?”`,
-  `"Any man who reads too much and uses his own brain too little falls into lazy habits of thinking."`,
-  `"If you don’t make mistakes, you’re not working on hard enough problems. And that’s a big mistake."`,
 ];
-let originators = [
-  "Seneca",
-  "Marcus Aurelius",
-  "Seneca",
-  "Albert Einstein",
-  "Frank Wilczek",
-];
+let originators = ["Seneca", "Marcus Aurelius"];
 
+// //Current number of quotes (by index number) [0, 1, 2, 3, 4]
 let quoteNumbers = [];
 for (let i = 0; i < quotes.length; i++) {
   quoteNumbers.push(i);
 }
 
+// // //Return a random number within number of quotes in array [0 - 4]
 function randomQuoteNumber() {
   let n = Math.floor(Math.random() * quotes.length);
   return quoteNumbers[n];
 }
 
-let previousQuote;
-let currentQuote;
-
 function generateQuote() {
-  let n = Math.floor(Math.random() * quotes.length);
-  quote.textContent = `${quotes[n]}`;
-  originator.textContent = `- ${originators[n]}`;
+  //Obtains current quote.
+  let currentQuote = document.querySelector(".quote").textContent;
+
+  //Sets next quote to random quote from the quotes array.
+  let nextQuote = quotes[randomQuoteNumber()];
+  //While both quotes are the same another quote is chosen at random.
+  while (currentQuote == nextQuote) {
+    nextQuote = quotes[randomQuoteNumber()];
+  }
+  //While both quotes are no longer matching a new quote is set.
+  quote.textContent = nextQuote;
 }
 
 generateButton.addEventListener("click", generateQuote);
-
-generateQuote();
