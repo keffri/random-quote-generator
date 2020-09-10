e0726bc995f62606b24ad6b6b97d08ceac7ecd75
keffri <keffri.w.neal@gmail.com>
2020-09-09 19:19:21 -0700

    cleaned up JavaScript file, and comments.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let quotes = [
   `Wisdom is the offspring of suffering and time`,
   `Let go your earthly tether. Enter the void. Empty and become wind.`,
 ];
+
 let originators = [
   `Seneca`,
   `Marcus Aurelius`,
@@ -21,18 +22,6 @@ let originators = [
   `Guru Laghima`,
 ];
 
-// //Current number of quotes/originators (by index number).
-let quoteNumbers = [];
-for (let i = 0; i < quotes.length; i++) {
-  quoteNumbers.push(i);
-}
-
-// // //Return a random number within number of quotes/originators in array.
-function randomQuoteNumber() {
-  let n = Math.floor(Math.random() * quotes.length);
-  return quoteNumbers[n];
-}
-
 //Sets the DOM for the quote and originator.
 let currentQuote = document.querySelector(".quote");
 let currentOriginator = document.querySelector(".originator");
@@ -41,36 +30,47 @@ let currentOriginator = document.querySelector(".originator");
 let copyQuotes = quotes.slice();
 let copyOriginators = originators.slice();
 
+//Boolean used to determine wether or not the button has been clicked recently.
 let buttonClicked = false;
-function generateRandom() {
-  //Sets the DOM of the about to be generated quote/originator.
-  let nextQuote = document.querySelector(".quote");
-  let nextOriginator = document.querySelector(".originator");
 
-  //If either array is empty, they will be reset.
+function generateRandom() {
+  //If the copied arrays are empty, then reset them to their original lengths.
   if (copyQuotes.length === 0 || copyOriginators.length === 0) {
     copyQuotes = quotes.slice();
     copyOriginators = originators.slice();
   }
 
-  //Obtains a random index number from the copy array. Varies depending on how many elements are left in the array.
+  //Obtains random index number from the amount of quotes/originators.
   let index = Math.floor(Math.random() * copyQuotes.length);
 
-  //Sets the quote and originator to the corresponding index number.
+  //Screen displays text appropriate to corresponding element selected from array index.
+  currentQuote.textContent = `"${copyQuotes[index]}"`;
+  currentOriginator.textContent = `${copyOriginators[index]}`;
+
+  //Adds a fade-in class with animation to change opacity.
   currentQuote.classList.add("fade-in");
   currentOriginator.classList.add("fade-in");
-  nextQuote.textContent = `"${copyQuotes[index]}"`;
-  nextOriginator.textContent = `${copyOriginators[index]}`;
 
-  //Removes the current quote and originator from the copy array.
+  //Removes the selected quote/originator from their arrays.
   copyQuotes.splice(index, 1);
   copyOriginators.splice(index, 1);
 
+  //Sets button status to true when clicked.
   buttonClicked = true;
+
   buttonOff();
 }
 
-//buttonClicked === true for 5 seconds after button is clicked, and then set to false.
+//If button has been clicked, remove its event listeners.
+function checkClicked() {
+  while (buttonClicked) {
+    generateButton.removeEventListener("click", generateRandom);
+    generateButton.removeEventListener("click", removeFade);
+    break;
+  }
+}
+
+//Five seconds after the button has been clicked readd event listeners, and set button status to false.
 function buttonOff() {
   if (buttonClicked === true) {
     setTimeout(() => {
@@ -81,14 +81,7 @@ function buttonOff() {
   }
 }
 
-function checkClicked() {
-  while (buttonClicked) {
-    generateButton.removeEventListener("click", generateRandom);
-    generateButton.removeEventListener("click", removeFade);
-    break;
-  }
-}
-
+//fade-in class removed 5 seconds after button is clicked.
 function removeFade() {
   setTimeout(() => {
     currentQuote.classList.remove("fade-in");
@@ -96,29 +89,6 @@ function removeFade() {
   }, 5000);
 }
 
-//Fade-in removed 5 seconds after button is clicked.
-
 generateButton.addEventListener("click", generateRandom);
 generateButton.addEventListener("click", removeFade);
 generateButton.addEventListener("click", checkClicked);
-
-// OLD METHOD USED FOR GENERATING RANDOM.
-// function generateQuote() {
-//   //Sets random index for quote in cases where current and next do not match.
-//   let c = randomQuoteNumber();
-//   //Obtains current quote.
-//   let currentQuote = document.querySelector(".quote").textContent;
-//   let currentOriginator = document.querySelector(".originator").textContent;
-//   //Sets next quote to random quote from the quotes array.
-//   let nextQuote = quotes[c];
-//   let nextOriginator = originators[c];
-//   //While both quotes are the same another quote is chosen at random.
-//   while (currentQuote == nextQuote || currentOriginator == nextOriginator) {
-//     let n = randomQuoteNumber();
-//     nextQuote = quotes[n];
-//     nextOriginator = originators[n];
-//   }
-//   //While both quotes are no longer matching a new quote is set.
-//   quote.textContent = nextQuote;
-//   originator.textContent = `- ${nextOriginator}`;
-// }
